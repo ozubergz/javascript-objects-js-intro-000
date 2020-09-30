57bdabb50d5d45c529f6740546dae6dca8ed1a40
James Min <jmin499@gmail.com>
2020-09-30 03:39:01 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,8 +1,14 @@
 var playlist = { "Future": "Mask off" }
-// var playlist = {}
 
+
+//destructive function
 function updatePlaylist(obj, artist, song) {
   // return Object.assign({}, obj, {[artist]: song})
   obj[artist] = song;
+  return obj
+}
+
+function(obj, artist) {
+  delete obj[artist];
   return obj
 }
